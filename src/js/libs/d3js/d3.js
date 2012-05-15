aca57ae261e7b7bdf1703a09415edf05f77d4266
Paul Bennett <mail@xerode.net>
2012-05-15 09:21:37 +0100

    Tweaking the wrapper for D3JS - now calls noConflict when it returns D3JS

--- src/js/libs/d3js/d3.js
+++ src/js/libs/d3js/d3.js
@@ -3,7 +3,7 @@ define(
         'order!libs/d3js/d3.v2.min'
     ],
     function() {
-        // Tell Require.js that this module returns a reference to jQuery
-        return d3;
+        // Tell Require.js that this module returns a reference to D3.js
+        return d3.noConflict();
     } 
 );
